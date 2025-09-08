04826fd00db1e8182a2d28519e16347d924d4e5b
Abito21 <abidjuliant@gmail.com>
2025-09-08 14:03:38 +0700

    Feat : Resume router for upload file

--- app/services/resume/resume_router.py
+++ app/services/resume/resume_router.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import APIRouter, Depends, UploadFile
 from sqlmodel import Session, select, desc
 from app.database.models import Resume
@@ -12,3 +13,19 @@ async def get_resumes(
     statement = select(Resume).order_by(desc(Resume.created_at))
     resumes = db.exec(statement).all()
     return resumes
+
+@resume_router.post("/")
+async def upload_resume(
+    file: UploadFile,
+    db: Session = Depends(db_session),
+):
+    contents = await file.read()
+    original_filename = file.filename or "unknown_file.pdf"
+    file_extension = os.path.splitext(original_filename)[1]
+
+    file_path = f"public/resumes/{original_filename}"
+    os.makedirs("public/resumes", exist_ok=True)
+    with open(file_path, "wb") as f:
+        f.write(contents)
+
+    return { "message": "Resume uploaded successfully" }
