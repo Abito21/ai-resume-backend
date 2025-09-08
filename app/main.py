e0c36a11df6978424a4236dbfcbaa5415550b3a7
Abito21 <abidjuliant@gmail.com>
2025-09-08 13:03:16 +0700

    Fix : Ignore secret .env

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ai-resume-backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from scalar_fastapi.scalar_fastapi import get_scalar_api_reference
 
+from app.core.settings import settings
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(
+    title=settings.app.APP_NAME,
+    version=settings.app.VERSION
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors.ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("scalar", include_in_schema=False)
+async def scalar():
+    return get_scalar_api_reference(
+        openapi_url=settings.app.OPENAPI_URL,
+        title=settings.app.APP_NAME
+    )
